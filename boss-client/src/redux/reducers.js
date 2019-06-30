1e639328ea984229f25b4a1a59e43e893966e48f
ChenYongYang <1214137301@qq.com>
2019-06-30 14:12:51 +0800

    pr

--- boss-client/src/redux/reducers.js
+++ boss-client/src/redux/reducers.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'
+import { AUTH_SUCCESS, ERROR_MSG } from './action-types'
 
 const initUser = {
     username: '',
@@ -6,8 +7,13 @@ const initUser = {
     msg: '' // 用来存储错误提示信息
 }
 
+// 用新数据来覆盖旧数据的
 function user(state=initUser, action) {
     switch(action.type){
+        case AUTH_SUCCESS:
+            return {...state, ...action.data}
+        case ERROR_MSG:
+            return {...state, msg: action.data}
         default:
             return state
     }
